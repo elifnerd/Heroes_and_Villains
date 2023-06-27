b28f9a8e31ab1cfd650c83be0deb123bf829831d
elifnerd <elisabethanne81@gmail.com>
2023-06-26 20:59:09 -0400

    wow, I forgot to commit! so far, I've added get by id, get all, post, and put in views.py for supers

--- super_type/views.py
+++ super_type/views.py
@@ -6,8 +6,11 @@ from .models import SuperType
 @api_view(['GET'])
 def super_type_list(request):
     super_types = SuperType.objects.all()
-    
     serializer = SuperTypeSerializer(super_types, many=True)
-    
-    
     return Response(serializer.data)
+
+@api_view(['GET'])
+def super_type_detail(request, pk):
+    
+    print(pk)
+    return Response((pk))
--- supers/views.py
+++ supers/views.py
@@ -1,13 +1,35 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import SuperSerializer
 from .models import Super
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def supers_list(request):
-    supers = Super.objects.all()
     
-    serializer = SuperSerializer(supers, many=True)
+    if request.method == 'GET':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(supers, many=True)
+        return Response(serializer.data)
     
+    elif request.method == 'POST':
+        supers = Super.objects.all()
+        serializer = SuperSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     
-    return Response(serializer.data)
+
+
+@api_view(['GET', 'PUT'])
+def super_detail(request, pk):
+    super = get_object_or_404(Super, pk=pk)
+    if request.method == 'GET':
+        serializer = SuperSerializer(super)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = SuperSerializer(super, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
